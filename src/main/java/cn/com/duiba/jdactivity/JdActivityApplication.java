50110785258a6eca53963889aab410c5f961612f
cengsp <cengshuiping@duiba.com.cn>
2021-05-12 15:15:24 +0800

    Initial commit

---
+++ src/main/java/cn/com/duiba/jdactivity/JdActivityApplication.java
@@ -0,0 +1,13 @@
+package cn.com.duiba.jdactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdActivityApplication.class, args);
+	}
+
+}
